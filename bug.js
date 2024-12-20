27529c88576889806c26ea1d639bedfefd3e660f
BugHunter-X <askzenai@gmail.com>
2024-12-20 06:02:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,27 @@
+```javascript
+const MongoClient = require('mongodb').MongoClient;
+
+async function run() {
+  try {
+    const uri = "mongodb+srv://<username>:<password>@<cluster-address>/<database>?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    await client.connect();
+    const database = client.db('mydatabase');
+    const collection = database.collection('mycollection');
+
+    // Insert a document with a nested array containing an object with a property that has a dot in its name
+    const doc = { a: [{ 'nested.property': 1 }] };
+    await collection.insertOne(doc);
+
+    // Query for the document using the dot notation to access the nested property
+    const foundDoc = await collection.findOne({ 'a.nested.property': 1 });
+    console.log(foundDoc);
+
+  } finally {
+    // Ensures that the client will close when you finish/
+    // with it
+    await client.close();
+  }
+}
+run().catch(console.dir);
+```
